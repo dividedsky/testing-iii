1107a9e423a7b9897179e40754fcd4d0b2691b4c
Justin <jdlow2@gmail.com>
2019-01-30 17:25:35 -0500

    tests for dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from "react";
+import { render } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("should show the controls and display", () => {
+    render(<Dashboard />);
+    const panel = document.querySelector(".controls");
+    const display = document.querySelector(".display");
+    expect(panel).toBeInTheDocument();
+    expect(display).toBeInTheDocument();
+  });
+});
